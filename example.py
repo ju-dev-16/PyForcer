7fa782d3446f44c3ab461d6be5e27966f5ad1a65
JuDev <jahidudd65@gmail.com>
2022-06-11 17:35:33 +0200

    Clean up and documented the code

--- example.py
+++ example.py
@@ -8,18 +8,21 @@ from string import ascii_letters, digits
 from random import choice
 
 from paramiko import SSHClient
-from paramiko.ssh_exception import AuthenticationException
 
 client = SSHClient()
 
-auth = Authentication(client, "C:\\Users\\JuDEV\\.ssh\\known_hosts")
+# Add your client instance and your known hosts path here
+auth = Authentication(client, known_hosts_path="C:\\Users\\JuDEV\\.ssh\\known_hosts")
 
 def try_to_login():  
     password = ''.join(choice(ascii_letters + digits) for _ in range(8))
 
-    auth.ssh_login(hostname="192.168.0.192", username="pi", password=password)
+    # Put the known and guessed informations here
+    auth.ssh_login(hostname="localhost", username="root", password=password)
     
     stdin, stdout, stderr = client.exec_command("hostname")
+    
+    # Debuging infos about the test command
     auth.debug_info(stdin, stdout, stderr)
         
     stdin.close()
@@ -29,8 +32,3 @@ def try_to_login():
 try_to_login()
 
 client.close()
-
-"""
-stdin.write("print(50 + 42)")
-stdin.channel.shutdown_write()
-"""
